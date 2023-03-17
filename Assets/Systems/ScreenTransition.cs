8c3b6b2a08849f1d166d1dc7abf8f1ea492805ca
Arman-Aslanyan <67847062+Arman-Aslanyan@users.noreply.github.com>
2023-03-16 17:59:46 -0700

    Scene Transitions done (Still need to get the Down and Right direction anims)

--- Assets/Systems/ScreenTransition.cs
+++ Assets/Systems/ScreenTransition.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.Animations;
 
 public class ScreenTransition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Animator transition;
+    public AnimatorController animControler;
+    public float transitionTime = 1f / 0.75f;
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.K))
+            Testing();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Testing()
     {
-        
+        PlayTransition("Down");
+    }
+
+    //Plays only the start of the given animation
+    public void StartTransition(string direction)
+    {
+        StartCoroutine(StartAnimation(direction));
+    }
+
+    //Plays only the end of the given animation
+    public void EndTransition(string direction)
+    {
+        StartCoroutine(EndAnimation(direction));
+    }
+
+    //Plays the entire animation given
+    public void PlayTransition(string direction)
+    {
+        StartTransition(direction);
+        EndTransition(direction);
+    }
+
+    private IEnumerator StartAnimation(string animation)
+    {
+        //transition.ResetTrigger(animation + "End");
+        transition.SetTrigger(animation + "Start");
+
+        yield return null;
+    }
+
+    private IEnumerator EndAnimation(string animation)
+    {
+        yield return new WaitForSeconds(transitionTime);
+
+        transition.SetTrigger(animation + "End");
     }
 }
